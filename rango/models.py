787a6dc1c506e4c3eaa4175814107ba4fafd3cf2
mcwilson91 <32679880+mcwilson91@users.noreply.github.com>
2018-01-28 11:54:38 +0000

    finished chapter 5

--- rango/models.py
+++ rango/models.py
@@ -8,8 +8,8 @@ class Category(models.Model):
 	class Meta:
 		verbose_name_plural = 'categories'
 
-		def __str__(self): 
-			return self.name
+	def __str__(self): 
+		return self.name
 
 
 class Page(models.Model):
